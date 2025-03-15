173068e8f61aaed35103d1a057d910aa568719b8
kshelp <kshelp@naver.com>
2025-03-16 07:03:40 +0900

    v1.2

---
+++ ch01_lab/src/test/java/org/zerock/club/ClubApplicationTests.java
@@ -0,0 +1,14 @@
+package org.zerock.club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("/*** contextLoads...");
+	}
+
+}
---
+++ ch01_lab/src/test/java/org/zerock/club/ClubApplicationTests2.java
@@ -0,0 +1,17 @@
+package org.zerock.club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.log4j.Log4j2;
+
+@SpringBootTest
+@Log4j2
+public class ClubApplicationTests2 {
+
+	@Test
+	void contextLoads() {
+		log.info("/*** contextLoads...");
+	}
+	
+}
